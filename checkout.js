0407ae19414f146a058ca610375c1438f76e68cf
abxyz99 <70295897+abxyz99@users.noreply.github.com>
2023-10-18 21:50:42 -0400

    added carousel and menu

--- checkout.js
+++ checkout.js
@@ -17,3 +17,20 @@
     }, false)
   })
 })()
+// only allow one selection per menu option
+document.addEventListener('DOMContentLoaded', function() {
+    const categories = document.querySelectorAll('.category');
+
+    categories.forEach(function(category) {
+        category.addEventListener('change', function(e) {
+            if (e.target.type === 'checkbox') {
+                const sameCategoryCheckboxes = category.querySelectorAll(`input[type="checkbox"]`);
+                sameCategoryCheckboxes.forEach(function(checkbox) {
+                    if (checkbox !== e.target) {
+                        checkbox.checked = false;
+                    }
+                });
+            }
+        });
+    });
+});
