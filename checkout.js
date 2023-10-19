3d0652e0e9f82a7ac3b01feed07a11b41e992e18
abxyz99 <70295897+abxyz99@users.noreply.github.com>
2023-10-18 22:02:33 -0400

    added menu checkboxes and form submission

--- checkout.js
+++ checkout.js
@@ -34,3 +34,19 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+// Existing JavaScript for checkbox selection
+document.addEventListener('DOMContentLoaded', function() {
+  const categories = document.querySelectorAll('.category');
+  categories.forEach(function(category) {
+    category.addEventListener('change', function(e) {
+      if (e.target.type === 'checkbox') {
+        const sameCategoryCheckboxes = category.querySelectorAll(`input[type="checkbox"]`);
+        sameCategoryCheckboxes.forEach(function(checkbox) {
+          if (checkbox !== e.target) {
+            checkbox.checked = false;
+          }
+        });
+      }
+    });
+  });
+});
